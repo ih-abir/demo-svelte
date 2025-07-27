3bd37c519087ddba459fc461fb954478daae1485
ih-abir <ih.abir.1@gmail.com>
2025-07-27 20:15:26 +0600

    config: remove redundant

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,9 @@
-import { imagetools } from 'vite-imagetools';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [
-		enhancedImages(),
-		imagetools({
-			defaultDirectives: (url) => {
-				if (url.searchParams.has('enhanced')) {
-					return new URLSearchParams({
-						format: 'avif;webp',
-						as: 'picture',
-						loading: 'lazy',
-						decoding: 'async',
-						sizes: '60;100;110;170;325;315;394;415;639;767;1023;1440;1920'
-					});
-				}
-				return new URLSearchParams();
-			}
-		}),
-		sveltekit()
-	],
+	plugins: [enhancedImages(), sveltekit()],
 	test: {
 		projects: [
 			{
